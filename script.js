5ef92e2aebb96edd56cde481528eb0b946861eb2
Alena <af@Alenas-MacBook-Pro.local>
2020-08-14 20:34:27 +0100

    logic

--- script.js
+++ script.js
@@ -2,5 +2,21 @@ let humanScore = 0;
 let computerScore = 0;
 let currentRoundNumber = 1;
 
-// Write your code below:
+function generateTarget(){
+ return Math.round(Math.random() * 10)
+};
 
+function compareGuesses(human, computer, target){
+  let humanAndTarget = human > target? human - target :  target - human;
+  let computerAndTarget = computer > target? computer - target : target - computer;
+  return humanAndTarget >= computerAndTarget
+}
+
+function updateScore(param){
+  if(param === 'human') ++humanScore;
+  else computerScore++;
+}
+
+function advanceRound(){
+  currentRoundNumber++;
+}
